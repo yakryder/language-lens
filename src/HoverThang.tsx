3d95574774a8ada3ed4bfd10f3c62ad9707ac9ab
SherSpock <wrtimberlake@gmail.com>
2020-09-26 20:43:03 -0400

    check da thing

---
+++ src/HoverThang.tsx
@@ -0,0 +1,15 @@
+import React, { useRef, useState, useEffect } from 'react';
+
+// /// Usage
+// export default function HoverThang() {
+//     const [isHovered, setIsHovered] = React.useState();
+  
+//     return (
+//       <div 
+//        onMouseEnter={()=> setIsHovered(true)}
+//        onMouseLeave={()=> setIsHovered(false)}
+//        >
+//         {isHovered ? '😁' : '☹️'}
+//       </div>
+//     );
+//   }
