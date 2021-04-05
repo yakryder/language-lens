aa5142e42d7fecf19068c442f8c1d808ad953e45
yakryder <14241275+yakryder@users.noreply.github.com>
2021-04-04 20:34:18 -0400

    Pivot
    Whole bunch of stuff changing, including the vision for the app. full-stack TS,  Vue front-end nestjs backend

--- test/acceptance/smoke.spec.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="Cypress" />
-
-const HOMEPAGE_TEXT = 'Edit';
-
-describe('The Home Page', () => {
-  it('successfully loads', () => {
-    cy.visit('http://localhost:3000').contains(HOMEPAGE_TEXT);
-  })
-})
